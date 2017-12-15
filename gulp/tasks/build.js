be71522be5ec37e11db280481272d733fb61d920
Qba <qba.skupinski@gmail.com>
2017-12-15 12:03:18 +0100

    guko build task

---
+++ gulp/tasks/build.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp'),
+imagemin = require('gulp-imagemin'),
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
+
+gulp.task('optimizeImages', ['icons'], function() {
+  return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+    .pipe(imagemin({
+      progressive: true,
+      interlaced: true,
+      multipass: true
+    }))
+    .pipe(gulp.dest("./dist/assets/images"));
+});
+
+gulp.task('usemin', ['styles', 'scripts'], function() {
+  return gulp.src("./app/index.html")
+    .pipe(usemin({
+      css: [function() {return rev()}, function() {return cssnano()}],
+      js: [function() {return rev()}, function() {return uglify()}]
+    }))
+    .pipe(gulp.dest("./dist"));
+});
+
+gulp.task('build', ['optimizeImages', 'usemin']);
